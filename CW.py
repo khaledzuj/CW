2a59d22c52d9231a966f7dbbcd7bb65f924ed8d7
khaledzuj <kmajumah@bradford.ac.uk>
2020-05-07 23:15:56 +0100

    final

--- CW.py
+++ CW.py
@@ -1,7 +1,7 @@
-fasta= ('/home/khaled/Downloads/KJ_problem/Cgr-B-H4-1G22_corrected_assembled_S-locus.fas')
-
-def translate(data):
-    """this function translates the nucleotides sequence in the data file into amino acid"""
+import sys
+file= (sys.argv[1])
+def translate_seq(data):
+    """this function translates the nucleotides sequence in data file into amino acid"""
     # create a dictionary containing the nucleotides with their translated amino acid
     genetic_code={'ATT':'I','ATC':'I','ATA':'I','CTT':'L','CTC':'L','CTA':'L','CTG':'L','TTA':'L','TTG':'L','GCA':'A', 'GCC':'A', 'GCG':'A', 'GCT':'A','GTA':'V', 'GTC':'V', 'GTG':'V', 'GTT':'V','TTC':'F', 'TTT':'F','ATG':'M','TGC':'C', 'TGT':'C','GGA':'G', 'GGC':'G', 'GGG':'G', 'GGT':'G','CCA':'P', 'CCC':'P', 'CCG':'P', 'CCT':'P','ACA':'T', 'ACC':'T', 'ACG':'T', 'ACT':'T','AGC':'S', 'AGT':'S', 'TCA':'S', 'TCC':'S', 'TCG':'S', 'TCT':'S','TAC':'Y', 'TAT':'Y','TGG':'W','CAA':'Q', 'CAG':'Q','AAC':'N', 'AAT':'N','CAC':'H', 'CAT':'H','GAA':'E', 'GAG':'E','GAC':'D', 'GAT':'D','AAA':'K', 'AAG':'K','AGA':'R', 'AGG':'R', 'CGA':'R', 'CGC':'R', 'CGG':'R', 'CGT':'R','TAA':'_','TAG':'_', 'TGA':'_'}
     protien_seq="" #empty string to add the amino acid to it
@@ -11,12 +11,12 @@ def translate(data):
     return protien_seq
 
 
-def get_gene(file,len_range_start,len_range_end,cys_count_range_start,cys_count_range_end):
+def get_protien_seq(file,len_range_start,len_range_end,cys_count_range_start,cys_count_range_end):
     """return the function to list of possible sequences that match the desired gene  criteria """
     with open(file, 'r') as data: # to parse the nucleotide sequence file to translate it
         for lines in data:
             if not lines.startswith('>'):
-                protien_seq=translate(lines[lines.find("ATG"):])# translate the amino acid and start reading from the first M
+                protien_seq=translate_seq(lines[lines.find("ATG"):])# translate the amino acid and start reading from the first M
 
     #the second step is to search for the desired gene
     k= 1000 #the size of the protein sequence imported each time to study
@@ -34,13 +34,13 @@ def get_gene(file,len_range_start,len_range_end,cys_count_range_start,cys_count_
 
 
 
-def blast_gene(data,database_type,base):
+def blast_protien_seq(data,database_type,base):
     """blast the genes sequince debend on (database_type,base) ,and produce an xML file"""
     import Bio
     from Bio.Blast import NCBIWWW # import nesseary pakeges
     result_handle = NCBIWWW.qblast(database_type, base, str(data)) # this code used to blast the sequences
     blast_result = result_handle.read()
-    return (blast_result)
+    print(blast_result)
 
 
-gene_blast= blast_gene(get_gene(file,60,150,6,9),"blastp","nr")
+blast_protien_seq(get_protien_seq(file,60,150,6,9),"blastp","nr")
